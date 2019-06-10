d0938a741aa99fc8a81c30953bdf5fb6b6c05aa7
Gilson da Gama <gilsondagama@hotmail.com>
2019-06-10 04:26:41 -0300

    Chat funcionando com vários clientes, com seleção de Logar / Cadastrar

---
+++ Servidor/src/com/meta/app/Servidor.java
@@ -0,0 +1,17 @@
+package com.meta.app;
+
+import com.meta.app.service.ServidorService;
+
+/**
+ *
+ * @author Gilson da Gama
+ */
+public class Servidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ServidorService();
+    }
+}
